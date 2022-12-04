9fa2d9c90004f41811ef1714931654356e5aaf57
Leifur <leifur00nielsen@gmail.com>
2022-12-04 23:32:35 +0100

    added css

--- header.php
+++ header.php
@@ -27,29 +27,19 @@
 
 <div id="page" class="hfeed site">
 	<NAV>
-		
 			<div class="nav-left">
 				<a href="">About</a>
 				<a href="#team">Team</a>
 				<a href="https://zuzana-gabonayova.com/esbjerg-sharks/shop/">Fanshop</a>
-
 			</div>
-
 			<div class="nav-center">
-				<a href="/"></a>
-				<img src="https://zuzana-gabonayova.com/esbjerg-sharks/wp-content/uploads/2022/12/logo_for_light_mode.png" alt="">
+				<a href="/"><img src="https://zuzana-gabonayova.com/esbjerg-sharks/wp-content/uploads/2022/12/logo_for_light_mode.png" alt=""></a>	
 			</div>
-
-
 			<div class="nav-right">
 				<a href="">Gallery</a>
 				<a href="#news">News</a>
 				<a href="#contact">Contact</a>
 			</div>
-		
-
-
-
 	</NAV>
 
 	<?php do_action( 'storefront_before_header' ); ?>
