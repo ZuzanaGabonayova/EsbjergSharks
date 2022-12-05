e9a1a26bf74d55a4158004c0fdd87b74d60ebc0c
Leifur <leifur00nielsen@gmail.com>
2022-12-05 02:30:11 +0100

    added css

--- blocks/lazyblock-coaches/block.php
+++ blocks/lazyblock-coaches/block.php
@@ -2,9 +2,9 @@
     <div class="coach-items">
         <?php foreach($attributes["coaches"] as $coaches): ?>
                 <div class="coach-item col-4 col-xs-12">
-                        <img src="<?php echo $coaches["img"]?>" alt="">
-                        <span class="coach-item__name"><?php echo $coaches["name"]?></span>
-                        <span class="coach-item__email"><?php echo $coaches["email"]?></span>
+                        <img class="fadeUpOnSeen" src="<?php echo $coaches["img"]?>" alt="">
+                        <span class="coach-item__name fadeUpOnSeen"><?php echo $coaches["name"]?></span>
+                        <span class="coach-item__email fadeUpOnSeen"><?php echo $coaches["email"]?></span>
                 </div>  
         <?php endforeach ?>
     </div>
